512930676f667cd2a470f421744f93127bf3cb2c
mysakli <misvancarova@gmail.com>
2026-01-29 22:28:27 +0100

    Revert "Merge pull request #3 from mysakli/Render-fixes"
    This reverts commit 48eccde63f910f0d8e374a4de8e5283d10384123, reversing
    changes made to 800ec1268fb7b052b6fe38265750ee3a01c3f2d1.

--- entries/models.py
+++ entries/models.py
@@ -5,7 +5,6 @@ from io import BytesIO
 from django.core.files.base import ContentFile
 from django.conf import settings
 import django_filters
-import os
 
 
 
@@ -45,7 +44,6 @@ class Meal(models.Model):
                 logger.info(f'=== SAVING MEAL: {self.name} ===')
                 logger.info(f'Photo storage backend: {type(self.photo.storage).__name__}')
                 logger.info(f'Photo storage class: {type(self.photo.storage)}')
-                logger.info(f'AWS_ACCESS_KEY_ID set: {os.getenv("AWS_ACCESS_KEY_ID")}')
                 
                 # Read file into memory before processing (needed for S3)
                 self.photo.file.seek(0)
